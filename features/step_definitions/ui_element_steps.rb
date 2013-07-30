f28ffb4240e89b3c80653f3a7a21cb774a9e202c
Randy Eppinger <RandyEppinger@neo.rr.com>
2013-07-29 20:51:26 -0400

    Refactored to use simpler frank selectors

--- features/step_definitions/ui_element_steps.rb
+++ features/step_definitions/ui_element_steps.rb
@@ -1,13 +1,13 @@
 Then(/^I see the color search textfield$/) do
-  check_element_exists "view:'UITextFieldCenteredLabel' marked:'#abcabc'"
+  check_element_exists "view marked:'#abcabc'"
 end
 Then(/^I see the search button$/) do
-  check_element_exists "view:'UIButtonLabel' marked:'Search'"
+  check_element_exists "view marked:'Search'"
 end
 When(/^I add ([^"]*)$/) do |tag|
   frankly_map( "textField placeholder:'tag'", "setText:", tag )
   step 'I touch "Add"'
 end
 Then(/^I see ([^"]*) is added to the list of tags$/) do |tag|
-  check_element_exists "view:'UILabel' marked:'#{tag}'"
+  check_element_exists "label marked:'#{tag}'"
 end
